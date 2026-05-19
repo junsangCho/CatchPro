fcf78bab98c1dbb25e5ae3a5f140bf529c15d06c
junsang.cho <wnstkddl90@naver.com>
2026-05-19 17:19:53 +0900

    Chore: 프로젝트 생성 및 스타터패키지 추가

---
+++ src/main/java/junsang/cho/catchpro/CatchProApplication.java
@@ -0,0 +1,13 @@
+package junsang.cho.catchpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/junsang/cho/catchpro/CatchProApplicationTests.java
@@ -0,0 +1,13 @@
+package junsang.cho.catchpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
